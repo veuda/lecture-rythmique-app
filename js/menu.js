4da9cf717bfad9a5e7b67cabc340a6c550a1c36a
veuda <62720180+veuda@users.noreply.github.com>
2021-01-27 17:52:25 +0100

    add

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,9 @@
 const menu = document.querySelector('.menu')
-const menuIcon = document.querySelector('.menu-icon')
+const menuIcon = document.querySelector('.js-icon')
 const links = document.querySelectorAll('.link')
 
 menuIcon.addEventListener('click', () => {
   menu.classList.toggle('open')
+  menuIcon.classList.toggle('fa-bars')
+  menuIcon.classList.toggle('fa-times')
 })
